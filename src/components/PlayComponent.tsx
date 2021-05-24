b994c4209f196a5444131ffcfe7e0c7096adbd06
MohirM <mohir591@gmail.com>
2021-05-24 11:55:11 +0200

    play version 1

---
+++ src/components/PlayComponent.tsx
@@ -0,0 +1,92 @@
+import React from "react";
+
+const PlayComponent: React.FC = () => {
+  return (
+    <div>
+      <link href="https://fonts.googleapis.com/css?family=Allerta" rel="stylesheet" />
+      <h2>Wellcome to Rofa Music store</h2>
+      <div className="container-audio">
+        <audio controls loop autoPlay>
+          <source
+            src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/9473/new_year_dubstep_minimix.ogg"
+            type="audio/ogg"
+          />
+          Your browser dose not Support the audio Tag
+        </audio>
+      </div>
+      {/* <div className="container-audio">
+        <div className="colum1">
+          <div className="row"></div>
+        </div>
+        <div className="colum1">
+          <div className="row"></div>
+        </div>
+        <div className="colum1">
+          <div className="row"></div>
+        </div>
+        <div className="colum1">
+          <div className="row"></div>
+        </div>
+        <div className="colum1">
+          <div className="row"></div>
+        </div>
+        <div className="colum1">
+          <div className="row"></div>
+        </div>
+        <div className="colum1">
+          <div className="row"></div>
+        </div>
+        <div className="colum1">
+          <div className="row"></div>
+        </div>
+        <div className="colum1">
+          <div className="row"></div>
+        </div>
+        <div className="colum1">
+          <div className="row"></div>
+        </div>
+        <div className="colum1">
+          <div className="row"></div>
+        </div>
+        <div className="colum1">
+          <div className="row"></div>
+        </div>
+        <div className="colum1">
+          <div className="row"></div>
+        </div>
+        <div className="colum1">
+          <div className="row"></div>
+        </div>
+        <div className="colum1">
+          <div className="row"></div>
+        </div>
+        <div className="colum1">
+          <div className="row"></div>
+        </div>
+        <div className="colum1">
+          <div className="row"></div>
+        </div>
+        <div className="colum1">
+          <div className="row"></div>
+        </div>
+        <div className="colum1">
+          <div className="row"></div>
+        </div>
+        <div className="colum1">
+          <div className="row"></div>
+        </div>
+        <div className="colum1">
+          <div className="row"></div>
+        </div>
+        <div className="colum1">
+          <div className="row"></div>
+        </div>
+        <div className="colum1">
+          <div className="row"></div> */}
+      {/* </div> */}
+      {/* </div> */}
+    </div>
+  );
+};
+
+export default PlayComponent;
